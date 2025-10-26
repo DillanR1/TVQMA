952cd2f33960c94d302f744aae0c42d7c5ccd5c9
dillan <dillanroby1@gmail.com>
2025-10-26 00:24:39 -0700

    Basic outline done. Nav items added. Basic style added

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('calendar-btn').addEventListener('click', () => {
+    alert('Live Google Calendar integration coming soon!');
+});
+
+document.getElementById('login-btn').addEventListener('click', () => {
+  alert('OAuth login integration planned for parent portal!');
+});
